08b0f10e9b758b51175dcab387bf7b7e3f64c605
HC-Vickers <1394568102@qq.com>
2020-04-06 00:45:13 +0800

    max

---
+++ Maxisum.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int count(int* a, int n)
+{
+	int thissum = 0, maxsum = 0;
+	for (int i = 0; i < n; i++) {
+		thissum += a[i];
+		if (thissum < 0)
+			thissum = 0;
+		if (thissum > maxsum)
+			maxsum = thissum;
+	}
+	return maxsum;
+} 
+int main()
+{
+	int n = 0;
+	int array[10000];
+	int thissum = 0, maxsum = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> array[i];
+	cout << count(array, n) << endl;
+	return 0;
+}
